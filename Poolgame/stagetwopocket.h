0f8a8029dd9c60af24a3a971184a0ce297c9d5e7
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-05-11 00:00:43 +1000

    lol

--- Poolgame/stagetwopocket.h
+++ Poolgame/stagetwopocket.h
@@ -3,14 +3,22 @@
 
 #include "pocket.h"
 
+/**
+ * @brief The StageTwoPocket class provides a concrete implementation
+ * of the Pocket interface
+ *
+ * @author nzho8446
+ * @since Stage 2
+ */
+
 class StageTwoPocket : public Pocket
 {
 public:
     StageTwoPocket(QVector2D pos, double radius);
 
-    ~StageTwoPocket() {}
+    virtual ~StageTwoPocket() {}
 
-    void render(QPainter &painter) override;
+    virtual void render(QPainter &painter) override;
 };
 
 #endif // STAGETWOPOCKET_H
